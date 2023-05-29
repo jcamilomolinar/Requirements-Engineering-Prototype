7c5d8541111b30a776602d3e9f5dd68ce5679950
David Lopez <92931033+Davidcito12@users.noreply.github.com>
2023-05-28 22:07:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,688 @@ function closeStealsBall() {
     window.style.display = "none";
     quitOverlay();
 }
+
+function openPassesBall() {
+    let window = document.getElementById("openPassesBall");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePassesBall() {
+    let window = document.getElementById("openPassesBall");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openModifiesScoreboard() {
+    let window = document.getElementById("openModifiesScoreboard");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeModifiesScoreboard() {
+    let window = document.getElementById("openModifiesScoreboard");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openModifiesPlayerStatistic() {
+    let window = document.getElementById("openModifiesPlayerStatistic");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeModifiesPlayerStatistic() {
+    let window = document.getElementById("openModifiesPlayerStatistic");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openModifiesTeaMStatistic() {
+    let window = document.getElementById("openModifiesTeaMStatistic");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeModifiesTeaMStatistic() {
+    let window = document.getElementById("openModifiesTeaMStatistic");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPurchasesTicket() {
+    let window = document.getElementById("openPurchasesTicket");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePurchasesTicket() {
+    let window = document.getElementById("openPurchasesTicket");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openAdmonishesPlAyer() {
+    let window = document.getElementById("openAdmonishesPlAyer");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeAdmonishesPlAyer() {
+    let window = document.getElementById("openAdmonishesPlAyer");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openCommitsFoul() {
+    let window = document.getElementById("openCommitsFoul");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeCommitsFoul() {
+    let window = document.getElementById("openCommitsFoul");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openDefenDsBasket() {
+    let window = document.getElementById("openDefenDsBasket");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeDefenDsBasket() {
+    let window = document.getElementById("openDefenDsBasket");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openSelectSTeam() {
+    let window = document.getElementById("openSelectSTeam");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeSelectSTeam() {
+    let window = document.getElementById("openSelectSTeam");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openControllingModifies() {
+    let window = document.getElementById("openControllingModifies");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeControllingModifies() {
+    let window = document.getElementById("openControllingModifies");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openIncreasIngScore() {
+    let window = document.getElementById("openIncreasIngScore");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeIncreasIngScore() {
+    let window = document.getElementById("openIncreasIngScore");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openGuaranteeinGRule() {
+    let window = document.getElementById("openGuaranteeinGRule");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeGuaranteeinGRule() {
+    let window = document.getElementById("openGuaranteeinGRule");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openEnhancingHighlight() {
+    let window = document.getElementById("openEnhancingHighlight");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeEnhancingHighlight() {
+    let window = document.getElementById("openEnhancingHighlight");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openIncreasIngPurchases() {
+    let window = document.getElementById("openIncreasIngPurchases");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeIncreasIngPurchases() {
+    let window = document.getElementById("openIncreasIngPurchases");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openMakingRevenue() {
+    let window = document.getElementById("openMakingRevenue");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeMakingRevenue() {
+    let window = document.getElementById("openMakingRevenue");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPromotingEvent() {
+    let window = document.getElementById("openPromotingEvent");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePromotingEvent() {
+    let window = document.getElementById("openPromotingEvent");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openProcuringAdmonishes() {
+    let window = document.getElementById("openProcuringAdmonishes");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeProcuringAdmonishes() {
+    let window = document.getElementById("openProcuringAdmonishes");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openAvoidingCommits() {
+    let window = document.getElementById("openAvoidingCommits");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeAvoidingCommits() {
+    let window = document.getElementById("openAvoidingCommits");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openImprovIngPlayerStatIstIc() {
+    let window = document.getElementById("openImprovIngPlayerStatIstIc");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeImprovIngPlayerStatIstIc() {
+    let window = document.getElementById("openImprovIngPlayerStatIstIc");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openReducingFoul() {
+    let window = document.getElementById("openReducingFoul");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeReducingFoul() {
+    let window = document.getElementById("openReducingFoul");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPlayerScoresBasket() {
+    let window = document.getElementById("openPlayerScoresBasket");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePlayerScoresBasket() {
+    let window = document.getElementById("openPlayerScoresBasket");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTicketSold() {
+    let window = document.getElementById("openTicketSold");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTicketSold() {
+    let window = document.getElementById("openTicketSold");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openSpectatorEntersSystem() {
+    let window = document.getElementById("openSpectatorEntersSystem");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeSpectatorEntersSystem() {
+    let window = document.getElementById("openSpectatorEntersSystem");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTeamStatisticVisualized() {
+    let window = document.getElementById("openTeamStatisticVisualized");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTeamStatisticVisualized() {
+    let window = document.getElementById("openTeamStatisticVisualized");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openCoachDirectsPlayer() {
+    let window = document.getElementById("openCoachDirectsPlayer");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeCoachDirectsPlayer() {
+    let window = document.getElementById("openCoachDirectsPlayer");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPlayerStatisticIncreased() {
+    let window = document.getElementById("openPlayerStatisticIncreased");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePlayerStatisticIncreased() {
+    let window = document.getElementById("openPlayerStatisticIncreased");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTeamStatisticUpdated() {
+    let window = document.getElementById("openTeamStatisticUpdated");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTeamStatisticUpdated() {
+    let window = document.getElementById("openTeamStatisticUpdated");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTicketAvailable() {
+    let window = document.getElementById("openTicketAvailable");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTicketAvailable() {
+    let window = document.getElementById("openTicketAvailable");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openFoulTypePersonalOrTechnical() {
+    let window = document.getElementById("openFoulTypePersonalOrTechnical");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeFoulTypePersonalOrTechnical() {
+    let window = document.getElementById("openFoulTypePersonalOrTechnical");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openScorer() {
+    let window = document.getElementById("openScorer");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeScorer() {
+    let window = document.getElementById("openScorer");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openScoreboard() {
+    let window = document.getElementById("openScoreboard");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeScoreboard() {
+    let window = document.getElementById("openScoreboard");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openArena() {
+    let window = document.getElementById("openArena");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeArena() {
+    let window = document.getElementById("openArena");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openSpectator() {
+    let window = document.getElementById("openSpectator");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeSpectator() {
+    let window = document.getElementById("openSpectator");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTicket() {
+    let window = document.getElementById("openTicket");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTicket() {
+    let window = document.getElementById("openTicket");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openAdministration() {
+    let window = document.getElementById("openAdministration");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeAdministration() {
+    let window = document.getElementById("openAdministration");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openCommissioner() {
+    let window = document.getElementById("openCommissioner");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeCommissioner() {
+    let window = document.getElementById("openCommissioner");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openOfficial() {
+    let window = document.getElementById("openOfficial");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeOfficial() {
+    let window = document.getElementById("openOfficial");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openGame() {
+    let window = document.getElementById("openGame");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeGame() {
+    let window = document.getElementById("openGame");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openSeason() {
+    let window = document.getElementById("openSeason");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeSeason() {
+    let window = document.getElementById("openSeason");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openNational-Basketball-Association() {
+    let window = document.getElementById("openNational-Basketball-Association");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeNational-Basketball-Association() {
+    let window = document.getElementById("openNational-Basketball-Association");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openConference() {
+    let window = document.getElementById("openConference");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeConference() {
+    let window = document.getElementById("openConference");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openEast() {
+    let window = document.getElementById("openEast");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeEast() {
+    let window = document.getElementById("openEast");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openWest() {
+    let window = document.getElementById("openWest");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeWest() {
+    let window = document.getElementById("openWest");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openEvent() {
+    let window = document.getElementById("openEvent");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeEvent() {
+    let window = document.getElementById("openEvent");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openAll-Star-Weekend() {
+    let window = document.getElementById("openAll-Star-Weekend");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeAll-Star-Weekend() {
+    let window = document.getElementById("openAll-Star-Weekend");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openReferee() {
+    let window = document.getElementById("openReferee");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeReferee() {
+    let window = document.getElementById("openReferee");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openEquipment() {
+    let window = document.getElementById("openEquipment");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeEquipment() {
+    let window = document.getElementById("openEquipment");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTeam() {
+    let window = document.getElementById("openTeam");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTeam() {
+    let window = document.getElementById("openTeam");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTrainer() {
+    let window = document.getElementById("openTrainer");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTrainer() {
+    let window = document.getElementById("openTrainer");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPlayer() {
+    let window = document.getElementById("openPlayer");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePlayer() {
+    let window = document.getElementById("openPlayer");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openCoach() {
+    let window = document.getElementById("openCoach");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeCoach() {
+    let window = document.getElementById("openCoach");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openFoul() {
+    let window = document.getElementById("openFoul");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeFoul() {
+    let window = document.getElementById("openFoul");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openTeamStatistic() {
+    let window = document.getElementById("openTeamStatistic");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeTeamStatistic() {
+    let window = document.getElementById("openTeamStatistic");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openGovernor() {
+    let window = document.getElementById("openGovernor");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeGovernor() {
+    let window = document.getElementById("openGovernor");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openSeasonStatistic() {
+    let window = document.getElementById("openSeasonStatistic");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeSeasonStatistic() {
+    let window = document.getElementById("openSeasonStatistic");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+function openPlayerStatistic() {
+    let window = document.getElementById("openPlayerStatistic");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closePlayerStatistic() {
+    let window = document.getElementById("openPlayerStatistic");
+    window.style.display = "none";
+    quitOverlay();
+}
+
+
+function openDatabaseAdministrator() {
+    let window = document.getElementById("openDatabaseAdministrator");
+    window.style.display = "block";
+    putOverlay();
+}
+
+function closeDatabaseAdministrator() {
+    let window = document.getElementById("openDatabaseAdministrator");
+    window.style.display = "none";
+    quitOverlay();
+}
