6ea680fb71aeeef943b2264e591de14acf4df9a8
Juan Camilo Molina <jmolinaro@unal.edu.co>
2023-05-28 20:39:32 -0500

    overlay

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+function putOverlay() {
+    let window = document.getElementById("overlay");
+    window.style.display = "block";
+}
+
+function quitOverlay() {
+    let window = document.getElementById("overlay");
+    window.style.display = "none";
+}
+
 
 function openStealsBall() {
-     let window = document.getElementById("openStealsBall")
-     window.style.display = "block"
+    let window = document.getElementById("openStealsBall");
+    window.style.display = "block";
+    putOverlay();
 }
 
 function closeStealsBall() {
-    let window = document.getElementById("openStealsBall")
-    window.style.display = "none"
+    let window = document.getElementById("openStealsBall");
+    window.style.display = "none";
+    quitOverlay();
 }
